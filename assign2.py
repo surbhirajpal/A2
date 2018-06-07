56bd52b7a6da14948ecd45b7ba5a1acf340a8277
surbhirajpal <surbhirajpal88@gmail.com>
2018-06-07 15:04:23 +0530

    assignment3

--- assign2.py
+++ assign2.py
@@ -1,27 +1,36 @@
-#Q1
-print("hello world")
+l=[]
+a=input("enter no")
+l.append(a)
+print(l)
 
-#Q2
-print("acad"+"view")
+#q2
+l.append("google")
+l.append("apple")
+l.append("facebook")
+l.append("microsoft")
+l.append("tesla")
+print(l)
 
-#Q3
-x=10
-y=20
-z=30
-print (x)
-print(y)
-print(z)
+#q3
+print(l.count("apple"))
 
-#Q4
-print("let\'s get started")
+#q4
+a=[2,5,7,8,4,2,4,6,8,9]
+a.sort()
+print(a)
 
-#Q5
-s="acadview"
-course="python"
-fees=5000
-print("hi,welcome to %s your course is %s and your fees is %d"%(s,course,fees))
+#q5
+a=[1,5,3,4,2]
+a.sort()
+b=[9,6,7,8]
+b.sort()
+c=[]
+c=a+b
+c.sort()
+print(c)
 
-#Q6
-name="tony stark "
-salary=1000000
-print(('%s''%d')%(name,salary))
+#q6
+c.append(10)
+print(c)
+c.pop()
+print(c)
